1e2f601fc54eaf122d27436e768370afb7f2ce28
anda53 <ciganaromungra@gmail.com>
2016-06-13 15:43:06 -0400

    second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,39 +4,34 @@ $( document ).ready(function() {
   var ansWrong = 0;
   var isRunning = false;
   var timeDisplay = $("<p></p>"); //creates p element
-  var counter = 20;
+  var counter = 30;
 
 
-
-  $("#test").hide();
+  $("scoreCard").hide();   //hide scorecard
+  $("#test").hide();       //hide test
   $("#start").click( function(){ 
-    $("#test").slideDown();
+  $("#test").slideDown();  //show test
 
-    if(isRunning==false){
      
-
       setInterval(function() {
-        counter--;
-        //console.log(counter);
-
+      counter--;
+      //console.log(counter);
         $('#topDiv').append(timeDisplay);  //shows where to put it
         $("p").html("Time Remaining:" + counter); 
            //console.log(counter);
-          if (counter >= 0) {
-            //console.log(counter);
             if (counter === 0) {
-              alert('sorry, out of time');
+              checkAnswer();
+              // alert('sorry, out of time');
+              $("#test").hide();
+              clearInterval(counter);
             };
-            clearInterval(counter);
-          } 
+       
+          
       }, 1000); 
-    }  
+     
+  });
 
-  }); 
-
-
-
-      var values = {
+ var values = {
         q1: '42',
         q2: 'paws',
         q3: 'false',
@@ -54,68 +49,44 @@ $( document ).ready(function() {
         q15: 'miniDaschund',
 
       }
-
-    
+//           //check each values item against the value of checked item
+        var checkAnswer = function(){
           $.each(values, function(name, value) {
               if($('input:radio[name="' + name + '"]:checked').val() == value){
                   ansCorrect++;
-                  console.log('The answer is correct.....!');
-                  $('#topDiv').html("Answers Correct:" + ansCorrect);
-
+                  console.log(name);
+                  console.log(value);
+                
 
               }else{
                   ansWrong++;
-                  console.log("the answer is incorrect damn...")
+                   
               }
+                $("p").hide();
+                $('#scoreCard').html("Answers Correct:" + ansCorrect);
+                $('#scoreCard').append(" Answers Wrong:" + ansWrong);
           });
-});     
-  // //function for if answer is right and adds 1 to score of each
-        // $('#submit').onsubmit(function(){
-
-        //   console.log('this isRunning working');
-        // })
-
-        
-      // $('#submit').click(function(){
-      //      if($('input:radio[name="' + name + '"]:checked').val() == value){
-      //             ansCorrect++;
-      //             console.log(ansCorrect);
-      //         }else{
-      //             ansWrong++;
-      //             console.log("the other answer is incorrect damn...")
-      //         }
-      // });
-
-    // make a function to check if the person won or not 
-
-
-      // function wonOrNot(){
-      //   // check all the answers
-      //   // alert u lose or u win
-      // }
-
-
-      // $("#submit").click( function(){
-      //     $('#topDiv').html("Total answers correct:" + ansCorrect);
-      //     $('#topDiv').html("Total answers wrong:" + ansWrong);
-      // });
- 
+        }
+});
 
 
 
 
-   
+    
 
 
 
-///Problem== function appends for every second in countdown
-//write a function that calculates wether ans is right or wrong
 //when all questions answered, calculate correct/wrong/unanswered answers
 // hide #test div
 //show div with correct, wrong, unnswered at end
    
 
 
+// Questions-
+// 1. counter stopping at 3
+ //2.program is computing all 15 questions wrong-why?
+// 3. show my score .html
+// 4. when counter is 0, .hide #test div and .show score 
 
 
 
